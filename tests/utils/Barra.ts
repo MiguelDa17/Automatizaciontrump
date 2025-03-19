0f7d215e1c0dc93c1dabef3f5c4ee2470aaf2120
MiguelDa17 <miguelda0156@gmail.com>
2025-03-19 13:53:49 -0500

    FixFunctionBarra

--- tests/utils/Barra.ts
+++ tests/utils/Barra.ts
@@ -6,19 +6,24 @@ import { Page, expect } from "@playwright/test";
  */
 export async function Barra(page: Page) {
   const menuButton = page.locator(".bg-pic-purple-light > .cursor-pointer");
-  await expect(menuButton).toBeVisible({ timeout: 10000 });
-  await menuButton.scrollIntoViewIfNeeded();
-  
-  // Hacer clic en el botón del menú
-  await menuButton.click();
-
-  // Esperamos un poco para dar tiempo a la animación
-  await page.waitForTimeout(1500); 
-
-  // Verificamos si el menú está visible, y si no, hacemos un segundo clic
   const menuVisible = page.locator(".sidenav-class"); // Ajusta la clase según sea necesario
+
+  // Esperar hasta que el botón del menú esté visible antes de hacer clic
+  await expect(menuButton).toBeVisible({ timeout: 20000 });
+
+  // Asegurar que el botón esté en la vista antes de hacer clic
+  await menuButton.scrollIntoViewIfNeeded();
+
+  // Hacer clic en el botón para abrir el menú
+  await menuButton.click();
+  await page.waitForTimeout(1500); // Espera por la animación
+
+  // Verificar si el menú se abrió, si no, hacer un segundo clic
   if (!(await menuVisible.isVisible())) {
-    await menuButton.waitFor({ state: 'visible', timeout: 5000 }); // Espera antes de hacer clic
+    await menuButton.waitFor({ state: "visible", timeout: 5000 }); // Esperar si el botón desaparece momentáneamente
     await menuButton.click();
-}
+  }
+
+  // Esperar hasta que el menú esté completamente visible
+  await expect(menuVisible).toBeVisible({ timeout: 10000 });
 }
